18cc6b096a9df9f2afb741190df66aa37a4d5ec0
march-dave <ysangy@gmail.com>
2017-06-18 14:23:40 -0400

    flex layer

--- index.ios.js
+++ index.ios.js
@@ -68,25 +68,51 @@ class HelloEveryone extends Component {
 
   render() {
     return (
-       <View style={{padding: 10}}>
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
           <Button
             onPress={this._onPressButton}
             title="Press Me"
           />
-        <TextInput
-          style={{height: 40}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="This looks great!"
+          />
+          <Button
+            onPress={this._onPressButton}
+            title="OK!"
+            color="#841584"
+          />
+        </View>
       </View>
-
-      );
+    );
   }
-
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
+
 
 
 AppRegistry.registerComponent('AwesomeProject2', () => HelloEveryone);
